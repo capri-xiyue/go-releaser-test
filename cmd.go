73a8ae435c5208bbb2d0768bab015a4d074b913c
XiyueYu <52932582+capri-xiyue@users.noreply.github.com>
2024-02-15 10:22:45 -0800

    debug print info

--- cmd.go
+++ cmd.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/abcxyz/pkg/buildinfo"
+	"runtime/debug"
 )
 
 func main() {
 	version := buildinfo.Version()
 	fmt.Println(version)
+	info, _ := debug.ReadBuildInfo()
+	fmt.Println(info)
 }
